ae7aa32b8787e9a406af25430bf41973616fcd0d
artn-dev <az-rtn@tutanota.com>
2023-09-10 22:34:24 -0300

    Replace temporary icons

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -17,6 +17,7 @@ import { useState } from 'react';
 import { encode } from 'base-64';
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 // If you deploy this to production, I will break into your house
 // and throw foam noodles at you for a week
@@ -39,8 +40,10 @@ const LoginOptionButton = ({optionName, icon, callback}) => {
 	        bg="#D9D9D9" rounded="full"
 	        alignItems="center" flexDirection="row"
 	      >
-	        {icon}
-	        <Text flex={1} ml={6}>Continuar com {optionName}</Text>
+	      	<Box size={21} alignItems="center" justifyContent="center">
+	        	{icon}
+	        </Box>
+	        <Text flex={1} ml={4}>Continuar com {optionName}</Text>
 	      </Box>
 		</Link>
 	)
@@ -84,9 +87,14 @@ const LoginForm = () => {
 					onChangeText={setPassword}
 					InputRightElement={
 						<Pressable onPress={() => setShowPassword(!showPassword)}>
-          					<CircleIcon color="#BEBBBB" mr="2" />
-          				</Pressable>
-      				}
+    					<Box mr="2">
+    						{ showPassword
+    							? <Ionicons name="eye-off-outline" color="gray" size={19}/>
+    							: <Ionicons name="eye-outline" color="gray" size={19}/>
+  							}
+    					</Box>
+    				</Pressable>
+  				}
 				/>
 			</FormControl>
 
@@ -113,10 +121,17 @@ export function Login() {
 			<Text fontSize="xl" color="black" mb={12} bold>Lorem ipsum dolor sit amet, consectetur.</Text>
 
 			<VStack space={3} w="70%">
-					<LoginOptionButton optionName="Apple" icon={<CircleIcon color="#BEBBBB" />} />
-					<LoginOptionButton optionName="Google" icon={<CircleIcon color="#BEBBBB" />} />
-					<LoginOptionButton optionName="Facebook" icon={<CircleIcon color="#BEBBBB" />} />
-					<LoginOptionButton optionName="e-mail" icon={<CircleIcon color="#BEBBBB" />}
+					<LoginOptionButton optionName="Apple"
+						icon={ <Ionicons name="logo-apple" color="black" size={16} /> } />
+
+					<LoginOptionButton optionName="Google"
+						icon={ <Ionicons name="logo-facebook" color="black" size={16} /> } />
+
+					<LoginOptionButton optionName="Facebook"
+						icon={ <Ionicons name="logo-google" color="black" size={16} /> } />
+
+					<LoginOptionButton optionName="e-mail"
+						icon={ <Ionicons name="mail-outline" color="black" size={16} /> }
 						callback={() => { setInLoginForm(true) }} />
 			</VStack>
 		</Box>
@@ -128,10 +143,10 @@ export function Login() {
 				<IconButton
 					position="absolute"
 					top="5"
-					left="5"
+					left="2"
 					onPress={() => setInLoginForm(!inLoginForm)}
 					borderRadius="full"
-					icon={<CircleIcon color="#BEBBBB" size="2xl" />}
+					icon={ <Ionicons name="arrow-back-circle-outline" color="gray" size={35} /> }
 				/>
 			}
 
