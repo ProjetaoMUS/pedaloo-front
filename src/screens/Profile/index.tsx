d846d9838d73dc2668ba02ee4a2920e9c4208e6f
artn-dev <az-rtn@tutanota.com>
2023-09-13 03:47:52 -0300

    Implement basic layout of profile page

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,12 +1,31 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { Box, Text, VStack } from 'native-base';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export function Profile({ navigation }) {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text
-                onPress={() => navigation.navigate('Home')}
-                style={{ fontSize: 26, fontWeight: 'bold' }}>Profile Screen</Text>
-        </View>
+        <LinearGradient
+            colors={['#32FC65', '#43F6B1']}
+            start={[0.2, 0]}
+            end={[1, 0.25]}
+            style={{
+                flex: 1,
+                alignItems: "center",
+                justifyContent: "center"
+            }}
+        >
+            <Box h="10%">
+                <Text fontSize={26}>Profile Buttons</Text>
+            </Box>
+            
+            <Box h="20%">
+                <Text fontSize={26} bold>Picture and name/email</Text>
+            </Box>
+
+            <VStack bg="white" w="100%" flex="1" alignItems="center" space={4} pt={7} borderTopRadius={60}>
+                <Text fontSize={26} bold>More Options</Text>
+            </VStack>
+        </LinearGradient>
     );
 }
+
