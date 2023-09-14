861cd32fd6ef6f758a855b5ebefa306918e9cb96
artn-dev <az-rtn@tutanota.com>
2023-09-13 21:47:00 -0300

    Retrieve user data from server

---
+++ src/api/user-data.tsx
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_BASE_URL = "YOUR_SERVER_ADDRESS_HERE";
+
+export const getUserData = async (id: number) => {
+	try {
+		const idStr = id.toString();
+		const res = await axios.get(API_BASE_URL + "users/" + idStr);
+		return res.data;
+		
+	} catch(err) {
+		console.log(res);
+	}
+
+	return null;
+}
