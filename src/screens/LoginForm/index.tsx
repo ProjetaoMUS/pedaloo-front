a5244eff4ca00f324b8ba325581ac293b968e100
artn-dev <az-rtn@tutanota.com>
2023-09-14 22:47:02 -0300

    Add login options and form as separate components

---
+++ src/screens/LoginForm/index.tsx
@@ -0,0 +1,75 @@
+import { Box, Image, FormControl, Input, Button, Pressable, IconButton } from 'native-base';
+import { useState } from 'react';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+import { performLogin } from '../../api/auth';
+
+const logo = require('../../../assets/green-logo.png');
+
+const PasswordInput = ({ value, onChangeText }) => {
+	const [showPassword, setShowPassword] = useState(false);
+
+	return (
+		<Input
+			type={showPassword ? "text" : "password"}
+			value={value}
+			onChangeText={onChangeText}
+			InputRightElement={
+				<Pressable onPress={() => setShowPassword(!showPassword)}>
+					<Box mr="2">
+						{showPassword
+							? <Ionicons name="eye-off-outline" color="gray" size={19} />
+							: <Ionicons name="eye-outline" color="gray" size={19} />
+						}
+					</Box>
+				</Pressable>
+			}
+		/>
+	);
+}
+
+const LoginForm = ({ onLogin })  => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+
+	return (
+		<Box flex={1} bg="white" alignItems="center">
+			<IconButton
+				position="absolute"
+				top="5"
+				left="2"
+				onPress={() => console.log("Pressed!")}
+				borderRadius="full"
+				icon={<Ionicons name="arrow-back-circle-outline" color="gray" size={35} />}
+			/>
+
+			<Image source={logo} resizeMode="contain" size={200} alt="Pedaloo" mt={12} />
+
+			<Box w="100%" px="10" alignItems="center">
+				<FormControl>
+					<FormControl.Label>e-mail</FormControl.Label>
+					<Input type="email" value={email} onChangeText={setEmail} />
+				</FormControl>
+
+				<FormControl mb="12">
+					<FormControl.Label>senha</FormControl.Label>
+					<PasswordInput />
+				</FormControl>
+
+				<Button
+					px="10"
+					bg="#D9D9D9"
+					_text={{ color: "black" }}
+					_pressed={{
+						bg: "#BEBBBB",
+						_text: { color: "muted.700" }
+					}}
+					onPress={() => performLogin(email, password) && onLogin()}
+				>
+					Entrar
+				</Button>
+			</Box>
+		</Box>
+	);
+}
---
+++ src/screens/LoginOptions/index.tsx
@@ -0,0 +1,48 @@
+import { VStack, Text, Box,	Image, Link } from 'native-base';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { performLogin } from '../../api/auth';
+
+const logo = require('../../../assets/green-logo.png');
+
+const LoginOptionButton = ({ optionName, icon, callback }) => {
+	return (
+		<Link onPress={callback}>
+			<Box w="100%" py="1.5" pl="2"
+				bg="#003714" rounded="full"
+				alignItems="center" flexDirection="row"
+			>
+				<Box size={21} alignItems="center" justifyContent="center">
+					{icon}
+				</Box>
+				<Text flex={1} color="white" ml={4}>Continuar com {optionName}</Text>
+			</Box>
+		</Link>
+	)
+};
+
+export function LoginOptions() {
+	return (
+		<Box flex={1} bg="white" alignItems="center">
+			<Image source={logo} resizeMode="contain" size={200} alt="Pedaloo" mt={12} />
+
+			<Box w="100%" alignItems="center" mt={-12}>
+				<Text fontSize="2xl" color="#003714" mb={1} bold>Estacione sua bicicleta</Text>
+				<Text fontSize="2xl" color="#003714" mb={10} bold>Perto & com segurança.</Text>
+
+				<VStack space={3} w="70%">
+					<LoginOptionButton optionName="Apple"
+						icon={<Ionicons name="logo-apple" color="white" size={16} />} />
+
+					<LoginOptionButton optionName="Google"
+						icon={<Ionicons name="logo-facebook" color="white" size={16} />} />
+
+					<LoginOptionButton optionName="Facebook"
+						icon={<Ionicons name="logo-google" color="white" size={16} />} />
+
+					<LoginOptionButton optionName="e-mail"
+						icon={<Ionicons name="mail-outline" color="white" size={16} />} />
+				</VStack>
+			</Box>
+		</Box>
+	)
+}
