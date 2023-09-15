b689ea3811100b43a44f4e93d238a975de933195
artn-dev <az-rtn@tutanota.com>
2023-09-15 16:17:26 -0300

    Add feedback logic to application

--- App.tsx
+++ App.tsx
@@ -1,25 +1,33 @@
 import { NativeBaseProvider } from 'native-base'
+import { FeedbackProvider } from './src/contexts/feedback';
 import { NavigationContainer } from '@react-navigation/native';
+import { useState } from 'react';
+
 import { Navbar } from './src/screens/Navbar';
 import { Login } from './src/screens/Login';
-import { useState } from 'react';
+import { Feedback } from './src/screens/Feedback';
+
 
 export default function App() {
   const [loggedIn, setLoggedIn] = useState(false);
 
-  if (loggedIn) {
-    return (
-    <NativeBaseProvider>
-      <NavigationContainer>
-        <Navbar />
-      </NavigationContainer>
-    </NativeBaseProvider>
-    )
-  }
-
   return(
     <NativeBaseProvider>
-      <Login onLogin={() => setLoggedIn(true)} />
+      <FeedbackProvider>
+        <Feedback />
+        { loggedIn
+          ? (
+            <NativeBaseProvider>
+              <NavigationContainer>
+                <Navbar />
+              </NavigationContainer>
+            </NativeBaseProvider>
+          )
+          : (
+            <Login onLogin={() => setLoggedIn(true)} />
+          )
+        }
+      </FeedbackProvider>
     </NativeBaseProvider>
 
     
