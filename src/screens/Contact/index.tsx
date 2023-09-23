61504d7d56a4ac2c271e8896bb217b3f7a53ded3
artn-dev <az-rtn@tutanota.com>
2023-09-23 18:54:46 -0300

    Remove contact page

--- src/screens/Contact/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Center, Text } from 'native-base';
-
-export function Contact() {
-	return (
-		<Center flex={1}>
-			<Text fontSize={24} color='#003714'>Contact</Text>
-		</Center>
-	);
-}
