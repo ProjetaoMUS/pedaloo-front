1ab9e7d2f465aba78e110a863b743bb3f371c937
artn-dev <az-rtn@tutanota.com>
2023-09-23 21:56:56 -0300

    Add help page basic layout

--- src/screens/Help/index.tsx
+++ src/screens/Help/index.tsx
@@ -1,9 +1,45 @@
-import { Center, Text } from 'native-base';
+import { VStack, Text, Button } from 'native-base';
 
 export function Help() {
 	return (
-		<Center flex={1}>
-			<Text fontSize={24} color='#003714'>Help</Text>
-		</Center>
+		<VStack flex={1} alignItems="center" space={5} bg="white">
+			<Text my={5} fontSize={12} color='#8F94A3'>Estamos disponíveis por estes meios de contato:</Text>
+
+			<Button
+				w="90%" py={5}
+				rounded="full"
+				bg="#32C000"
+				_text={{
+					color: "white",
+					fontSize: 14,
+					fontWeight: "bold"
+				}}
+				_pressed={{
+					bg: "#299900",
+					_text: { color: "muted.200" }
+				}}
+				onPress={() => {}}
+			>
+				Whatsapp
+			</Button>
+
+			<Button
+				w="90%" py={5}
+				rounded="full"
+				bg="#32C000"
+				_text={{
+					color: "white",
+					fontSize: 14,
+					fontWeight: "bold"
+				}}
+				_pressed={{
+					bg: "#299900",
+					_text: { color: "muted.200" }
+				}}
+				onPress={() => {}}
+			>
+				Email
+			</Button>
+		</VStack>
 	);
 }
