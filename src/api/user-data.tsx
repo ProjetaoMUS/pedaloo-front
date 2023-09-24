b89fa5aa8f65f8abd731ccb687309dfc3e553293
artn-dev <az-rtn@tutanota.com>
2023-09-24 02:25:27 -0300

    Add mock api function to update profile information in server

--- src/api/user-data.tsx
+++ src/api/user-data.tsx
@@ -14,3 +14,21 @@ export const getUserData = async (id: number) => {
 
 	return null;
 }
+
+export const updateUserData = async (id: number, data) => {
+	try {
+		const idStr = id.toString();
+		const url = API_BASE_URL + "users/" + idStr + "/";
+		console.log(`Updating account information in route ${url}
+New account info:
+	first_name: ${data.first_name}
+	email: ${data.email}
+	phone_number: ${data.phone_number}`);
+		return true;
+
+	} catch(err) {
+		console.log(err)
+	}
+
+	return null;
+}
