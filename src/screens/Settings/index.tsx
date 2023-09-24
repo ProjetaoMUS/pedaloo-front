d923bcfa196452a0be18b7f4997fb8ac767ca82b
artn-dev <az-rtn@tutanota.com>
2023-09-23 21:49:32 -0300

    Implement basic settings page layout

--- src/screens/Settings/index.tsx
+++ src/screens/Settings/index.tsx
@@ -1,9 +1,49 @@
-import { Center, Text } from 'native-base';
+import { Center, Text, Button, VStack } from 'native-base';
 
 export function Settings() {
 	return (
-		<Center flex={1}>
-			<Text fontSize={24} color='#003714'>Settings</Text>
+		<Center flex={1} bg="white">
+			<Center w="100%" h="72%">
+				<Text fontSize={14} color='black'>[edit password]</Text>
+			</Center>
+
+			<VStack flex={1} w="100%" space={3} alignItems="center">
+				<Button
+					w="80%" py={5}
+					rounded="full"
+					bg="white"
+					_text={{
+						color: "#696969",
+						fontSize: 14,
+						fontWeight: "bold"
+					}}
+					_pressed={{
+						bg: "white",
+						_text: { color: "muted.400" }
+					}}
+					onPress={() => {}}
+				>
+					Delete Account
+				</Button>
+
+				<Button
+					w="80%" py={5}
+					rounded="full"
+					bg="#FF5050"
+					_text={{
+						color: "white",
+						fontSize: 14,
+						fontWeight: "bold"
+					}}
+					_pressed={{
+						bg: "#f65a5a",
+						_text: { color: "muted.200" }
+					}}
+					onPress={() => {}}
+				>
+					Disconnect
+				</Button>
+			</VStack>
 		</Center>
 	);
 }
