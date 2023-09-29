b5314f6109a5c9d0e5bb684f3834d37367d6c695
Gabriel Queiroz <gqmv@cin.ufpe.br>
2023-09-29 20:42:14 -0300

    Added fucking env vars

--- src/api/user-data.tsx
+++ src/api/user-data.tsx
@@ -1,34 +1,33 @@
-import axios from 'axios';
+import { API_URL } from "@env";
+import axios from "axios";
 
-const API_BASE_URL = "http://192.168.87.23:8000/api/";
+const API_BASE_URL = API_URL;
 
 export const getUserData = async (id: number) => {
-	try {
-		const idStr = id.toString();
-		const res = await axios.get(API_BASE_URL + "users/" + idStr);
-		return res.data;
-		
-	} catch(err) {
-		console.log(err);
-	}
+  try {
+    const idStr = id.toString();
+    const res = await axios.get(API_BASE_URL + "users/" + idStr);
+    return res.data;
+  } catch (err) {
+    console.log(err);
+  }
 
-	return null;
-}
+  return null;
+};
 
 export const updateUserData = async (id: number, data) => {
-	try {
-		const idStr = id.toString();
-		const url = API_BASE_URL + "users/" + idStr + "/";
-		console.log(`Updating account information in route ${url}
+  try {
+    const idStr = id.toString();
+    const url = API_BASE_URL + "users/" + idStr + "/";
+    console.log(`Updating account information in route ${url}
 New account info:
 	first_name: ${data.first_name}
 	email: ${data.email}
 	phone_number: ${data.phone_number}`);
-		return true;
+    return true;
+  } catch (err) {
+    console.log(err);
+  }
 
-	} catch(err) {
-		console.log(err)
-	}
-
-	return null;
-}
+  return null;
+};
