0099a3a1d61a993eb135a839abaebe5b2725dcbe
artn-dev <az-rtn@tutanota.com>
2023-10-01 22:45:46 -0300

    Fix api functions

--- src/api/auth.tsx
+++ src/api/auth.tsx
@@ -2,31 +2,23 @@ import { encode } from "base-64";
 import { api } from "./config";
 import { saveData } from "./local-storage";
 
-export const performLogin = async (
-  email: string,
-  password: string
-): Promise<void> => {
+export const performLogin = async (email: string, password: string): Promise<void> => {
   if (!global.btoa) {
     global.btoa = encode;
   }
 
   try {
-    const url = `${API_BASE_URL}auth/login/`;
-    const response = await axios.post(
-      url,
-      {},
-      {
-        auth: {
-          username: email,
-          password: password,
-        },
-      }
-    );
+    const response = await api.post('auth/login/', {}, {
+      auth: {
+        username: email,
+        password: password,
+      },
+    });
 
     await saveData("token", response.data.token);
     return response.data;
 
   } catch (err) {
-    console.log(err);
+    console.error(err);
   }
 };
